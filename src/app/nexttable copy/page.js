db5af41abbf395577718fe7c322c669236eb8615
Neeraj19 <19futuretech@gmailcom>
2024-04-17 20:59:11 +0530

    add

---
+++ src/app/nexttable copy/page.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PivotTable from './PivotTable';
+
+export default function Nexttable() {
+    const data = [
+        { rowLabel: 'Row 1', columnLabel: 'Column 1', value: 10 },
+        { rowLabel: 'Row 1', columnLabel: 'Column 2', value: 20 },
+        { rowLabel: 'Row 3', columnLabel: 'Column 1', value: 30 },
+        { rowLabel: 'Row 2', columnLabel: 'Column 2', value: 40 },
+        { rowLabel: 'Row 2', columnLabel: 'Column 3', value: 50 },
+      ];
+  return (
+    <div>
+      <h1>Pivot Table Example</h1>
+      <PivotTable data={data} />
+      
+    </div>
+  )
+}
