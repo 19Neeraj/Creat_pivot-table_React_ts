d8ae9fe05ba6f4313e3f7b3091ae19e53bfc693e
19Neeraj <19futuretech@gmail.com>
2024-06-06 16:08:36 +0530

    .

--- src/app/components/try.js
+++ src/app/components/try.js
@@ -4,42 +4,53 @@
 //     const {data}=props;
 //     let typeForSUM_weight = "SomeNewType";
 //     let typeForTotal = data;
-  
+
 //     // Generate the array of objects with dynamically changing types
 //     let arrayOfObjects = [
 //       { name: "SUM_revenue", caption: "revenue", type: "PercentageOfGrandTotal" },
 //       { name: "SUM_weight", caption: "weight", type: typeForSUM_weight },
 //       { name: "Total", caption: "Total Units %", type: typeForTotal }
 //     ];
-  
+
 //     return arrayOfObjects;
 //   }
-  
+
 //   // Usage example
 //   let arrayOfObjects = generateArrayOfObjects( data='arraydata');
 //   console.log(arrayOfObjects);
 
-
-
 const arrayOfObjects = [
-    { name: "SUM_revenue", caption: "revenue", type: "PercentageOfGrandTotal" },
-    { name: "SUM_weight", caption: "weight" },
-    { name: "Total", caption: "Total Units %" }
-  ];
-  
-  // Define the value you want to assign to the type key
-  const newTypeValue = "the change index";
-  
-  // Loop through each object in the array and add the type key with the new value
+  { name: "SUM_revenue", caption: "revenue", type: "PercentageOfGrandTotal" },
+  { name: "SUM_weight", caption: "weight" },
+  { name: "Total", caption: "Total Units %" },
+];
+
+// Define the value you want to assign to the type key
+const newTypeValue = "the change index";
+
+// Loop through each object in the array and add the type key with the new value
 //   arrayOfObjects.forEach(obj => {
 //     obj.type = newTypeValue;
 //   });
 
-
 // change only index 0
 for (let i = 0; i < arrayOfObjects.length; i++) {
-    arrayOfObjects[i].type = newTypeValue;
-  }
-  
-  console.log(arrayOfObjects);
-  
+  arrayOfObjects[i].type = newTypeValue;
+}
+
+console.log(arrayOfObjects);
+
+let a = 2;
+var b = 5;
+const c = 6;
+
+function Check() {
+  b = 40;
+  // c = 10;
+  a = 40;
+  console.log(a, b);
+}
+
+
+Check();
+console.log(a,b,c);
